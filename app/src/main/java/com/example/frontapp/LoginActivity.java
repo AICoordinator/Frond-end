5bd6c8da4852c96b3fd614187bf8eae477d173dc
Seo-sang <tjtkdgus45@nate.com>
2022-03-16 18:11:35 +0900

    add signup activity not complete yet

--- app/src/main/java/com/example/frontapp/LoginActivity.java
+++ app/src/main/java/com/example/frontapp/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.example.frontapp;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signUpBtn = (Button)findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
